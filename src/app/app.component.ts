238b0c86981104874361993ccaf3a092710617e4
alinabushynskaja <alina.bushinska@smartexe.com>
2022-05-26 11:50:59 +0300

    fix formGroup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { DataService } from './core/services/data.service';
 import * as data from './core/form.json';
 import { Model, PatientQuestions } from './core/models/model';
@@ -10,8 +10,8 @@ import { Model, PatientQuestions } from './core/models/model';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  data!:any;
-  form!: FormGroup;
+  data!: any;
+  form: FormGroup = this.fb.group({});
   title = 'test-project';
 
   constructor(private fb: FormBuilder, public dataService: DataService) { }
@@ -19,17 +19,29 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     this.data = this.dataService.data;
 
-    this.form = this.fb.group({
-      question: ['', Validators.required],
-      age: [],
-      medicine: [],
-      pills: [],
-      water: [],
-      answerWater: []
+
+    this.data.patientQuestions.forEach((el: any) => {
+      this.createControls(el);
     });
   }
 
+  createControls(el: any): void {
+    if (!el.formControlName) {
+      return;
+    }
+
+    this.form.addControl(el.formControlName, new FormControl(''));
+
+    if (el.childItems) {
+      el.childItems.forEach((item: any) => {
+        this.createControls(item)
+      });
+    }
+  }
+
   submit() {
+    console.log(this.form);
+
     this.dataService.sendForm(this.form)
   }
 }
